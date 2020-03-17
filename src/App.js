5049b8d7b31bfd94f0108000ed19cb6b0790d8cf
Andrius Bacianskas <a.bacianskas@gmail.com>
2020-03-17 09:39:33 +0100

    Eslint configurations and style enforcement

--- src/App.js
+++ src/App.js
@@ -1,69 +1,61 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { TweenMax, TweenLite, TimelineMax, Back } from 'gsap';
-import * as ScrollMagic from 'scrollmagic';
-import { ReactComponent as Logo } from './assets/images/insignis-logo.svg';
-import { ReactComponent as ContactUs } from './assets/images/contact-us.svg';
-import { ReactComponent as BrandIdentities } from './assets/images/brand-identities.svg';
-import { ReactComponent as Websites } from './assets/images/websites.svg';
-import { ReactComponent as AndMore } from './assets/images/more.svg';
-import './sass/main.scss';
-import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+import React, { useEffect, useState } from 'react'
+import { TweenMax, TimelineMax, Back } from 'gsap'
+import * as ScrollMagic from 'scrollmagic'
+import { ScrollMagicPluginGsap } from 'scrollmagic-plugin-gsap'
+import { ReactComponent as Logo } from './assets/images/insignis-logo.svg'
+import { ReactComponent as ContactUs } from './assets/images/contact-us.svg'
+import { ReactComponent as BrandIdentities } from './assets/images/brand-identities.svg'
+import { ReactComponent as Websites } from './assets/images/websites.svg'
+import { ReactComponent as AndMore } from './assets/images/more.svg'
+import './sass/main.scss'
 
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax, Back)
 
 function App() {
-  const brandTween = useRef(null)
-
   const [backgroundColor, setBackgroundColor] = useState('default')
 
   useEffect(() => {
-    const controller = new ScrollMagic.Controller();
+    const controller = new ScrollMagic.Controller()
     const brandTimeline = new TimelineMax()
-    let brandTween = TweenMax.fromTo(
-      '#brand-tween', 
-      3, 
-      {y: 300, x: 500, width: 200, height: 120, opacity: 0}, 
-      {y: 0, x: 100, width: 300, height: 190, opacity: 1}
-    );
+    const brandTween = TweenMax.fromTo(
+      '#brand-tween',
+      3,
+      { y: 300, x: 500, width: 200, height: 120, opacity: 0 },
+      { y: 0, x: 100, width: 300, height: 190, opacity: 1 }
+    )
 
-    let brandTween1 = TweenMax.fromTo(
-      '#brand-tween1', 
-      5, 
-      {x: 500, y: 350, width: 200, height: 120, opacity: 0}, 
-      {x: 800, y: -100, width: 300, height: 190, opacity: 1, delay: 1}
-    );
+    const brandTween1 = TweenMax.fromTo(
+      '#brand-tween1',
+      5,
+      { x: 500, y: 350, width: 200, height: 120, opacity: 0 },
+      { x: 800, y: -100, width: 300, height: 190, opacity: 1, delay: 1 }
+    )
 
-    let brandTween2 = TweenMax.fromTo(
-      '#brand-tween2', 
-      5, 
-      {x: 500, y: 400, width: 200, height: 120, opacity: 0}, 
-      {x: 1200, y: 800, width: 300, height: 190, opacity: 1, delay: 2}
-    );
+    const brandTween2 = TweenMax.fromTo(
+      '#brand-tween2',
+      5,
+      { x: 500, y: 400, width: 200, height: 120, opacity: 0 },
+      { x: 1200, y: 800, width: 300, height: 190, opacity: 1, delay: 2 }
+    )
 
     brandTimeline.add([brandTween, brandTween1, brandTween2])
 
-    new ScrollMagic.Scene(
-      {
-        triggerElement: "#brand", 
-        duration: 1200,
-        offset: 400
-      })
-      .setPin("#brand-pin")
+    new ScrollMagic.Scene({
+      triggerElement: '#brand',
+      duration: 1200,
+      offset: 400,
+    })
+      .setPin('#brand-pin')
       .setTween(brandTimeline)
-      .addTo(controller);
-    new ScrollMagic.Scene(
-      {
-        triggerElement: "#websites", 
-        duration: 600,
-        offset: 400,
-      })
-      .setPin("#websites")
-      .addTo(controller);
+      .addTo(controller)
+    new ScrollMagic.Scene({
+      triggerElement: '#websites',
+      duration: 600,
+      offset: 400,
+    })
+      .setPin('#websites')
+      .addTo(controller)
   }, [])
-
-  const onHover = (color) => {
-    setBackgroundColor(color)
-  }
 
   return (
     <div className={`App default ${backgroundColor}`}>
@@ -90,18 +82,19 @@ function App() {
         <div className="row">
           <div id="hero" className="header-text">
             <div className="main-header">
-              We are a group of independent designers and developers focused on creating outstanding
+              We are a group of independent designers and developers focused on
+              creating outstanding
             </div>
           </div>
           <div id="brand" className="brand-text">
-          <div 
-                id="brand-tween" 
-                className="tween" 
-                onMouseEnter={() => setBackgroundColor('blue')} 
-                onMouseLeave={() => setBackgroundColor((''))}
-              ></div>
-              <div id="brand-tween1" className="tween"></div>
-              <div id="brand-tween2" className="tween"></div>
+            <div
+              id="brand-tween"
+              className="tween"
+              onMouseEnter={() => setBackgroundColor('blue')}
+              onMouseLeave={() => setBackgroundColor('')}
+            />
+            <div id="brand-tween1" className="tween" />
+            <div id="brand-tween2" className="tween" />
             <div id="brand-pin">
               <BrandIdentities />
             </div>
@@ -114,6 +107,7 @@ function App() {
         </div>
       </section>
     </div>
-  );
-  }
-export default App;
+  )
+}
+
+export default App
