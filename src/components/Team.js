708827ce04228c4fa44d803fb0f8b365b8b7cd22
Andrius Bacianskas <a.bacianskas@gmail.com>
2020-03-22 14:52:24 +0100

    Added client logos in Team comp

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import teamMember from '../assets/images/teamMember.png'
+import Amazon from '../assets/images/amazon.svg'
+import Google from '../assets/images/google.svg'
+import Mtv from '../assets/images/mtv.svg'
+import Eyem from '../assets/images/eyem.svg'
+import Virgin from '../assets/images/virgin.svg'
+import perfectWellness from '../assets/images/perfectWellness.svg'
+import oppo from '../assets/images/oppo.svg'
+import cn from '../assets/images/cn.svg'
+import mijia from '../assets/images/mijia.svg'
+import decathlon from '../assets/images/decathlon.svg'
+import dzine from '../assets/images/dzine.svg'
+import dayOne from '../assets/images/dayOne.svg'
 
 const Team = props => {
   const { title } = props
@@ -11,18 +23,18 @@ const Team = props => {
       <div className="container">
         <div className="row">
           <div className="image">
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
-            <img src={teamMember} alt="name" />
+            <img src={Amazon} alt="Amazon kindle" />
+            <img src={Google} alt="Google" />
+            <img src={Mtv} alt="Mtv" />
+            <img src={Eyem} alt="Eyem" />
+            <img src={Virgin} alt="Virgin" />
+            <img src={perfectWellness} alt="Perfect Wellness" />
+            <img src={oppo} alt="Oppo" />
+            <img src={cn} alt="Cartoon network" />
+            <img src={mijia} alt="Mijia" />
+            <img src={decathlon} alt="Decathlon" />
+            <img src={dzine} alt="Dzine elements" />
+            <img src={dayOne} alt="Day One" />
           </div>
         </div>
       </div>
