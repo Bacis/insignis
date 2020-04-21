7012a4791122bad4f1fccd5cefa6c54b53ef295e
Andrius Bacianskas <a.bacianskas@gmail.com>
2020-04-21 21:53:38 +0200

    feat: added website examples

--- src/components/Websites.js
+++ src/components/Websites.js
@@ -3,70 +3,95 @@ import { TweenMax, TimelineMax } from 'gsap'
 import PropTypes from 'prop-types'
 import * as ScrollMagic from 'scrollmagic'
 
+import eclipseImg from '../assets/images/eclipse-c.png'
+import medicineHelpImg from '../assets/images/medicine-help.png'
+import perfectWellnessImg from '../assets/images/perfect-wellness.png'
+import tattooInkImg from '../assets/images/tattoo-ink.png'
+
 const Websites = props => {
   const { setBackgroundColor, controller } = props
 
-  const webTweenRef = React.createRef()
-  const webTweenRef1 = React.createRef()
-  const webTweenRef2 = React.createRef()
+  const eclipseRef = React.createRef()
+  const medicineHelpRef = React.createRef()
+  const perfectWellnessRef = React.createRef()
+  const tattooInkRef = React.createRef()
 
   useEffect(() => {
     const webTimeline = new TimelineMax()
     const centerY = window.innerHeight / 10
-    const centerX = window.innerWidth / 2
     const webTween = TweenMax.fromTo(
-      webTweenRef.current,
+      eclipseRef.current,
       6,
-      { y: centerY, x: centerX, width: 200, height: 120, opacity: 0, delay: 4 },
-      { y: -600, x: 100, width: 300, height: 190, opacity: 1, delay: 4 }
+      { y: centerY, x: 0, width: 200, height: 120, opacity: 0, delay: 4 },
+      { y: -600, x: -800, width: 400, height: 250, opacity: 1, delay: 4 }
     )
 
     const webTween1 = TweenMax.fromTo(
-      webTweenRef1.current,
+      medicineHelpRef.current,
       3,
       {
-        x: centerX,
+        x: 0,
         y: centerY,
         width: 200,
         height: 120,
         opacity: 0,
       },
       {
-        x: centerX + 1000,
-        y: centerY - 500,
-        width: 300,
-        height: 190,
+        x: 800,
+        y: centerY - 800,
+        width: 400,
+        height: 250,
         opacity: 1,
         delay: 5,
       }
     )
 
     const webTween2 = TweenMax.fromTo(
-      webTweenRef2.current,
+      perfectWellnessRef.current,
       7,
       {
-        x: centerX,
+        x: 0,
         y: centerY,
         width: 200,
         height: 120,
         opacity: 0,
       },
       {
-        x: centerX + 1000,
+        x: 1000,
         y: centerY + 100,
-        width: 300,
-        height: 190,
+        width: 400,
+        height: 250,
         opacity: 1,
         delay: 5,
       }
     )
 
-    webTimeline.add([webTween, webTween1, webTween2])
+    const webTween3 = TweenMax.fromTo(
+      tattooInkRef.current,
+      8,
+      {
+        x: 0,
+        y: centerY,
+        width: 200,
+        height: 120,
+        opacity: 0,
+      },
+      {
+        x: -1000,
+        y: centerY + 200,
+        width: 400,
+        height: 250,
+        opacity: 1,
+        delay: 4,
+      }
+    )
+
+    webTimeline.add([webTween, webTween1, webTween2, webTween3])
 
     new ScrollMagic.Scene({
       triggerElement: '#websites',
       offset: 0,
-      duration: 900,
+      duration: 5000,
     })
       .setPin('#web-pin')
       .setTween(webTimeline)
@@ -76,20 +101,34 @@ const Websites = props => {
   return (
     <div id="websites" className="brand-text">
       <div id="web-pin">
-        <div
-          ref={webTweenRef}
+        <img
+          src={eclipseImg}
+          alt="Eclipse website"
+          ref={eclipseRef}
           className="tween"
           onMouseEnter={() => setBackgroundColor('blue')}
           onMouseLeave={() => setBackgroundColor('')}
         />
-        <div
-          ref={webTweenRef1}
+        <img
+          src={medicineHelpImg}
+          alt="Medicine help website"
+          ref={medicineHelpRef}
           className="tween"
           onMouseEnter={() => setBackgroundColor('orange')}
           onMouseLeave={() => setBackgroundColor('')}
         />
-        <div
-          ref={webTweenRef2}
+        <img
+          src={perfectWellnessImg}
+          alt="Perfect wellness website"
+          ref={perfectWellnessRef}
+          className="tween"
+          onMouseEnter={() => setBackgroundColor('red')}
+          onMouseLeave={() => setBackgroundColor('')}
+        />
+        <img
+          src={tattooInkImg}
+          alt="Tattoo ink website"
+          ref={tattooInkRef}
           className="tween"
           onMouseEnter={() => setBackgroundColor('red')}
           onMouseLeave={() => setBackgroundColor('')}
