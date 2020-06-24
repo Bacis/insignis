1ccdf0a5b0f69f9912e850d8a9cab3bd8d5ebd7e
Andrius Bacianskas <a.bacianskas@gmail.com>
2020-06-24 16:53:54 +0200

    added video file in contact us footer component

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,47 +1,76 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import blurVideo from '../assets/Neon-blue.mp4'
 
 const ContactUs = () => {
   const [background, setBackground] = useState('')
+  const windowHeight = window.innerHeight
+  let videoRef
+
+  useEffect(() => {
+    videoRef = document.getElementById('blurVideo')
+  })
+
+  const hoverLeave = () => {
+    videoRef.pause()
+    setBackground('video-fadeout')
+  }
+
+  const hoverOn = () => {
+    videoRef.play()
+    setBackground('video-fadein')
+  }
 
   return (
-    <div className={`footer ${background}`}>
-      <div
-        className="section-header"
-        onMouseEnter={() => setBackground('blur')}
-        onMouseLeave={() => setBackground('')}
-      >
-        Contact us
-      </div>
-      <div className="grid-footer">
-        <div className="column">
-          <div className="city">Berlin</div>
-          <div className="address">
-            <p>Holzmarktstrasse 64</p>
-            <p>10179 — Berlin</p>
-            <p>DE +49 1577 1587 382</p>
+    <>
+      <video
+        src={blurVideo}
+        type="video/mp4"
+        id="blurVideo"
+        loop
+        preload="none"
+        className={`${background}`}
+        height={windowHeight}
+        width={window.innerWidth}
+      />
+      <div className="footer">
+        <div
+          className="section-header"
+          onMouseEnter={() => hoverOn()}
+          onMouseLeave={() => hoverLeave()}
+        >
+          Contact us
+        </div>
+        <div className="grid-footer">
+          <div className="column">
+            <div className="city">Berlin</div>
+            <div className="address">
+              <p>Holzmarktstrasse 64</p>
+              <p>10179 — Berlin</p>
+              <p>DE +49 1577 1587 382</p>
+            </div>
+          </div>
+          <div className="column">
+            <div className="city">Athens</div>
+            <p>Sokratous 6,</p>
+            <p>15562 — Cholargos</p>
+            <p>GR +306978455765</p>
+          </div>
+          <div className="column">
+            <div className="city">Genoa</div>
+            <p>Via San Fruttuoso 2</p>
+            <p>16143 — Genoa</p>
+            <p>IT +39 333 617 0799</p>
           </div>
         </div>
-        <div className="column">
-          <div className="city">Athens</div>
-          <p>Sokratous 6,</p>
-          <p>15562 — Cholargos</p>
-          <p>GR +306978455765</p>
-        </div>
-        <div className="column">
-          <div className="city">Genoa</div>
-          <p>Via San Fruttuoso 2</p>
-          <p>16143 — Genoa</p>
-          <p>IT +39 333 617 0799</p>
+        <div className="grid-footer-social">
+          <div className="social">Behance</div>
+          <div className="social">Dribble</div>
+          <div className="social">Facebook</div>
+          <div className="social">Instagram</div>
         </div>
       </div>
-      <div className="grid-footer-social">
-        <div className="social">Behance</div>
-        <div className="social">Dribble</div>
-        <div className="social">Facebook</div>
-        <div className="social">Instagram</div>
-      </div>
-    </div>
+    </>
   )
 }
 
